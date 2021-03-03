367e6fb858f7f9f917a626f35da731fc74672f23
jayrdeaton <jayrdeaton@gmail.com>
2021-03-03 14:42:30 -0600

    cleaned up language, added options to metadata

--- src/program/index.js
+++ src/program/index.js
@@ -3,18 +3,22 @@ const { command, option } = require('termkit'),
 
 const program = command('cleanup')
   .version(process.env.npm_package_version)
-  .description('A cli for cleaning up files and directories given criteria.  \nThis could potentially be dangerous if you don\'t know what you are doing or aren\'t careful.  \n\nUSE AT YOUR OWN RISK!  \nYOU HAVE BEEN WARNED!')
+  .description('A cli for cleaning up your file system.  \nThis could potentially be dangerous if you don\'t know what you are doing or aren\'t careful.  \n\nUSE AT YOUR OWN RISK!  \nYOU HAVE BEEN WARNED!')
   .commands([
     command('junk', '[dir]')
+      .description('Clean junk files and directories that match given criteria')
       .option('i', 'includes', '<str>', 'Delete items with names that include a given string')
       .option('e', 'excludes', '<str>', 'Delete items with names that exclude a given string')
       .option(null, 'extension', '<str>', 'Delete files with a given extension')
       .option('s', 'size', '<num>', 'Delete items under given size in megabytes')
       // .option('R', 'recursive', null, 'Scan directories recursively')
       .option('v', 'verbose', null, 'Display more info')
-      .option(null, 'force', null, 'Forego confirmation')
+      .option('f', 'force', null, 'Forego confirmation')
       .action(junk),
     command('metadata', '[dir]')
+      .description('Clean file title and comment metadata')
+      .option('v', 'verbose', null, 'Display more info')
+      .option('f', 'force', null, 'Forego confirmation')
       .action(metadata)
   ])
 
